c4ae71905631e67edc0059158af98f48e3534f8b
ejovrh <hrvoje.ivanic@gmx.net>
2018-05-28 14:29:51 +0200

    pin swap in the schematic! - gpio_definitions.h: MOSI moved from PB5 to PB6 and MISO moved from PB6 to PB5, corrected the comments - gpio_modes.h: updated MISO/MOSI pin swap, set green/red LED to be off(high) by default, activated CAN transceiver in order to debug CAN code

--- MJ808/MJ808/gpio_modes.h
+++ MJ808/MJ808/gpio_modes.h
@@ -2,16 +2,16 @@
 #define GPIO_MODES_H_
 
 gpio_conf(MCP2515_INT_pin, INPUT, LOW);
-gpio_conf(MCP2561_standby_pin, OUTPUT, HIGH);						// low (on), high (off)
+gpio_conf(MCP2561_standby_pin, OUTPUT, LOW);						// low (on), high (off)
 
-gpio_conf(SPI_MISO_pin, INPUT, LOW);
-gpio_conf(SPI_MOSI_pin, OUTPUT, LOW);
+gpio_conf(SPI_MOSI_pin, OUTPUT, LOW); // FIXME: pins are swapped, needs to be updated in the schematic
+gpio_conf(SPI_MISO_pin, INPUT, LOW); // FIXME: pins are swapped, needs to be updated in the schematic
 gpio_conf(SPI_SCK_pin, OUTPUT, HIGH);
 gpio_conf(SPI_SS_MCP2515_pin, OUTPUT, HIGH);					// //high (device inert), low (device selected)
 
 gpio_conf(PWM_front_light_pin, OUTPUT, LOW);						// low (off), high (on)
-gpio_conf(RED_LED_pin, OUTPUT, LOW);										// low (on), high (off)
-gpio_conf(GREEN_LED_pin, OUTPUT, LOW);									// low (on), high (off)
+gpio_conf(RED_LED_pin, OUTPUT, HIGH);										// low (on), high (off)
+gpio_conf(GREEN_LED_pin, OUTPUT, HIGH);									// low (on), high (off)
 gpio_conf(PUSHBUTTON_pin, INPUT, HIGH);								//
 
 
