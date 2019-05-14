0c6a4f1253a02d05278fd0a288dfa56166ac212c
ejovrh <hrvoje.ivanic@gmx.net>
2019-05-14 19:31:22 +0200

    - whitespace changes in comments

--- MJ808/MJ808/gpio_modes.h
+++ MJ808/MJ808/gpio_modes.h
@@ -1,18 +1,18 @@
 #ifndef GPIO_MODES_H_
 #define GPIO_MODES_H_
 
-gpio_conf(MCP2515_INT_pin, INPUT, HIGH);				// INT1, active low
-gpio_conf(MCP2561_standby_pin, OUTPUT, LOW);		// low (on), high (off)
+gpio_conf(MCP2515_INT_pin, INPUT, HIGH);								// INT1, active low
+gpio_conf(MCP2561_standby_pin, OUTPUT, LOW);							// low (on), high (off)
 
-gpio_conf(ICSP_DO_MOSI, OUTPUT, LOW);						// data out - output pin
-gpio_conf(ICSP_DI_MISO, INPUT, LOW);						// data in - input pin
-gpio_conf(SPI_SCK_pin, OUTPUT, LOW);						// low for proper CPOL = 0 waveform
-gpio_conf(SPI_SS_MCP2515_pin, OUTPUT, HIGH);		// //high (device inert), low (device selected)
+gpio_conf(ICSP_DO_MOSI, OUTPUT, LOW);									// data out - output pin
+gpio_conf(ICSP_DI_MISO, INPUT, LOW);									// data in - input pin
+gpio_conf(SPI_SCK_pin, OUTPUT, LOW);									// low for proper CPOL = 0 waveform
+gpio_conf(SPI_SS_MCP2515_pin, OUTPUT, HIGH);							// //high (device inert), low (device selected)
 
-gpio_conf(PWM_front_light_pin, OUTPUT, LOW);		// low (off), high (on)
-gpio_conf(RED_LED_pin, OUTPUT, HIGH);						// low (on), high (off)
-gpio_conf(GREEN_LED_pin, OUTPUT, HIGH);					// low (on), high (off)
-gpio_conf(PUSHBUTTON_pin, INPUT, LOW);					// SPST-NO - high on press, low on release
+gpio_conf(PWM_front_light_pin, OUTPUT, LOW);							// low (off), high (on)
+gpio_conf(RED_LED_pin, OUTPUT, HIGH);									// low (on), high (off)
+gpio_conf(GREEN_LED_pin, OUTPUT, HIGH);									// low (on), high (off)
+gpio_conf(PUSHBUTTON_pin, INPUT, LOW);									// SPST-NO - high on press, low on release
 
 
 #endif /* GPIO_MODES_H_ */
--- MJ808/MJ808/uci_spi.c
+++ MJ808/MJ808/uci_spi.c
@@ -35,20 +35,20 @@
 // slave select must happen outside of this function
 uint8_t spi_uci_transfer(const uint8_t data)
 {
-	cli(); // lets call it paranoia but i want the SPI transaction to be atomic - i.e. no IRQ shall be able to abort it
-	USIDR = data;									// put the payload into the USI data register
+	cli();									// lets call it paranoia but i want the SPI transaction to be atomic - i.e. no IRQ shall be able to abort it
+	USIDR = data;							// put the payload into the USI data register
 
 	USISR = _BV(USIOIF);					// counter overflow interrupt, indicates counter overflow
 
 	do
 	{
-		USICR = ( _BV(USIWM0)	// select three wire mode
-						 |_BV(USICS1)	// positive edge - CPHA0
-						 |_BV(USICLK)	// clock strobe
+		USICR = ( _BV(USIWM0)				// select three wire mode
+						 |_BV(USICS1)		// positive edge - CPHA0
+						 |_BV(USICLK)		// clock strobe
 						 |_BV(USITC));		// generates the clock by toggling the SCK pin
-	}	// due to clocking the USIDR is shifted into the slave (and data received)
-	while ((USISR & _BV( USIOIF) ) == 0 ); // this loop provides the software defined clock; this all is SW SPI...
+	}										// due to clocking the USIDR is shifted into the slave (and data received)
+	while ((USISR & _BV( USIOIF) ) == 0 );	// this loop provides the software defined clock; this all is SW SPI...
 
 	sei();
-	return USIDR;									// hopefully have something useful here...
+	return USIDR;							// hopefully have something useful here...
 }
