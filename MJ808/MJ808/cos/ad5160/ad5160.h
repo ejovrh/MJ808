9cb1a45f39287db3f66916892e42f291cada0d15
hrvoje ivanic <hrvoje.ivanic@gmx.net>
2019-12-09 16:11:07 +0100

    in file ad5160.c and .h: - wrote private static void __set_wiper(const uint8_t byte) which transfers a byte into the poti and thereby sets a resistance. 1 LSB = 60R, see datasheet pp.13 - set function pointer for void (* SetWiper)(const uint8_t in_val); to above private func.

--- MJ808/MJ808/cos/ad5160/ad5160.h
+++ MJ808/MJ808/cos/ad5160/ad5160.h
@@ -3,9 +3,7 @@
 
 typedef struct ad5160_t													// mcp23s08_t struct describing the port expander as a whole
 {
-
+	void (* SetWiper)(const uint8_t in_val);							// sets resistor wiper to numerical value, 60R per LSB
 } ad5160_t __attribute__((aligned(8)));
-
-ad5160_t *ad5160_ctor();												// mcp23s08_t object constructor - does function pointer & hardware initialization
 
 #endif /* AD5160_H_ */
