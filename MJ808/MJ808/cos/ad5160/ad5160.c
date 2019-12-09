f9a4b47b19ee865fa84f315f22e5ea657a04534c
hrvoje ivanic <hrvoje.ivanic@gmx.net>
2019-12-09 16:11:07 +0100

    in file ad5160.c and .h: - wrote private static void __set_wiper(const uint8_t byte) which transfers a byte into the poti and thereby sets a resistance. 1 LSB = 60R, see datasheet pp.13 - set function pointer for void (* SetWiper)(const uint8_t in_val); to above private func.

--- MJ808/MJ808/cos/ad5160/ad5160.c
+++ MJ808/MJ808/cos/ad5160/ad5160.c
@@ -1,25 +1,27 @@
-//#include <avr/interrupt.h>
-//#include <avr/sleep.h>
-//#include <avr/io.h>
-//#include <util/delay.h>
-//#include <avr/pgmspace.h>
+#include <avr/io.h>
+#include <util/delay.h>
 
 #include "gpio.h"
 //#include "uci_spi.h"
 #include "cos\ad5160\ad5160.h"
 
-#define	SPI_SS_AD5160_pin		D,	5,	5								// AD5160 Poet Expander Slave Select
+#define	SPI_SS_AD5160_pin		D,	5,	5								// AD5160 Port Expander Slave Select
 
 typedef struct															// ad5160_t actual
 {
 	ad5160_t public;													// public struct
-
 } __ad5160_t;
 
-
-ad5160_t * ad5160_ctor()
+// sets resistor value by sending a 1-byte value; see datasheet pp. 13; 1 LSB = 60R
+static void __set_wiper(const uint8_t in_val)
 {
-	;
-}
+	gpio_clr(SPI_SS_AD5160_pin);										// select the slave
+	spi_uci_transfer(in_val);											// send the byte
+	_delay_us(1);
+	gpio_set(SPI_SS_AD5160_pin);										// de-select the slave
+};
 
-extern __ad5160_t __AD5160;												// declare ad5160_t actual
+__ad5160_t __AD5160 =													// instantiate ad5160_t actual and set function pointers
+{
+	.public.SetWiper = &__set_wiper										// set up function pointer for public methods
+};
