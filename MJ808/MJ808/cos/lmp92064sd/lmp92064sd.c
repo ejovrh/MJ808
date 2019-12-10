73cb8df0b6f35f804226a3cb518f77ea3443d7dc
ejovrh <hrvoje.ivanic@gmx.net>
2019-12-10 08:28:09 +0100

    in file lmp92064sd.c and .h: - started to write driver for device in file mcp2515.c: - indentation changes

--- MJ808/MJ808/cos/lmp92064sd/lmp92064sd.c
+++ MJ808/MJ808/cos/lmp92064sd/lmp92064sd.c
@@ -1,7 +1,7 @@
 //#include <avr/interrupt.h>
 //#include <avr/sleep.h>
-//#include <avr/io.h>
-//#include <util/delay.h>
+#include <avr/io.h>
+#include <util/delay.h>
 //#include <avr/pgmspace.h>
 
 #include "gpio.h"
@@ -14,7 +14,21 @@ typedef struct															// lmp92064sd_t actual
 {
 	lmp92064sd_t public;												// public struct
 
+	uint8_t	__data_out[4];												// read 4 bytes from 0x0203 down
 } __lmp92064sd_t;
+
+static void __read_values(uint8_t *data)
+{
+	uint8_t i;
+
+	gpio_clr(SPI_SS_LMP92064SD_pin);									// select the slave
+	for (i = 0; i<4; ++i)												// while the SS is held, the address is auto-decremented, thus multiple bytes can be read
+	{
+		*(data+i) = spi_uci_transfer(0x0203-i);							// get the result
+		_delay_us(1);													// delay a little bit for the transfer to complete
+	}
+	gpio_set(SPI_SS_LMP92064SD_pin);									// de-select the slave
+};
 
 
 lmp92064sd_t * lmp92064sd_ctor()
