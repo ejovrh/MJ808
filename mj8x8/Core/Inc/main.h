dd57ff0e43dfc8a9892a9343e36731b9f7404fe3
ejovrh cinavi <hrvoje.ivanic@gmx.net>
2022-12-17 22:19:01 +0100

    - created bare bones C project based on mj8x8-template - removed CubeIDE comments - imported project config from mj8x8-template

---
+++ mj8x8/Core/Inc/main.h
@@ -0,0 +1,10 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include "stm32f0xx_hal.h"	// include HAL layer
+
+#define MJ808	// what device to compile for
+
+void Error_Handler(void);  //Exported functions prototypes
+
+#endif /* __MAIN_H */
---
+++ mj8x8/Core/Src/stm32f0xx_hal_msp.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+void HAL_MspInit(void)	// Initializes the Global MSP
+{
+	__HAL_RCC_SYSCFG_CLK_ENABLE();
+	__HAL_RCC_PWR_CLK_ENABLE();
+}
