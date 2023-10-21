bb265e6ac51c8e99fccd1b87d7fada0f5723adea
ejovrh cinavi <hrvoje.ivanic@gmx.net>
2023-10-21 14:52:02 +0200

    - introduced arrays _RegOffset[] and _RegSize[] along with enum _RegSize which together specify register offset, register size and index - implemented __ReadWrapper(uint16_t MemAddress, uint16_t MemAddSize, uint8_t *pData), which does the actual I2C bus read traffic - implemented _Read(const uint8_t RegAddr), which makes use of above wrapper - implemented _Dump(void), which makes use of above read function

--- BQ25798/Core/Src/bq25798.c
+++ BQ25798/Core/Src/bq25798.c
@@ -4,58 +4,108 @@ typedef struct	// bq25798_t actual
 {
 	bq25798_t public;  // publicly visible struct
 
-	uint8_t *__buffer;	// internal buffer for I2C device address and payload data (both r and w)
+	uint16_t *__buffer;  // internal buffer for I2C device address and payload data (both r and w)
 	I2C_HandleTypeDef *__hi2c;	// I2C HAL handler
 } __bq25798_t;
 
-static __bq25798_t   __Device   __attribute__ ((section (".data")));	// preallocate __Device object in .data
+static __bq25798_t __Device __attribute__ ((section (".data")));	// preallocate __Device object in .data
 
 extern void Error_Handler(void);
-extern I2C_HandleTypeDef hi2c1;
-uint8_t _buf[49];  // internal array for register address & payload
-//
-void __Read(const uint8_t RegAddr, const uint8_t len)
+
+uint16_t _buf[REG_CNT];  // internal array for register address & payload
+
+static const uint8_t _RegOffset[REG_CNT] =  // offset of each register address
+	{  //
+	0x0, 0x1, 0x3, 0x5, 0x6, 0x7, 0x8, 0xA, 0xB, 0xD,  // REG00 - REG0D
+	0xE, 0xF, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,  // REG0E - REG17
+	0x18, 0x19, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F, 0x20, 0x21, 0x22,  // REG18 - REG22
+	0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2A, 0x2B, 0x2C,  // REG23 - REG2C
+	0x2D, 0x2E, 0x2F, 0x30, 0x31, 0x33, 0x35, 0x37, 0x39, 0x3B,  // REG2D - REG3B
+	0x3D, 0x3F, 0x41, 0x43, 0x45, 0x47, 0x48	// REG3D - REG48
+	};
+
+static const uint8_t _RegSize[REG_CNT] =  // size of each register address
+	{  // 1 or 2 bytes
+	1, 2, 2, 1, 2, 1, 1, 1, 2, 1,  // 01 - 10 -- REG 0x00 - REG 0x0D
+	1, 1, 1, 1, 1, 1, 1, 1, 1, 1,  // 11 - 20 -- REG 0x0E - REG 0x17
+	1, 2, 1, 1, 1, 1, 1, 1, 1, 1,  // 21 - 30 -- REG 0x18 - REG 0x22
+	1, 1, 1, 1, 1, 1, 1, 1, 1, 1,  // 31 - 40 -- REG 0x23 - REG 0x2C
+	1, 1, 1, 1, 2, 2, 2, 2, 2, 2,  // 41 - 50 -- REG 0x2D - REG 0x3B
+	2, 2, 2, 2, 2, 1, 1  //						51 - 57 -- REG 0x3D - REG 0x48
+	};
+
+// wrapper for I2C read procedure
+static void __ReadWrapper(uint16_t MemAddress, uint16_t MemAddSize, uint8_t *pData)
 {
-	do	// read from device
+	do  // read from device
 		{
-			if(HAL_I2C_Mem_Read_DMA(&hi2c1, (uint16_t) BQ25798_I2C_ADDR, RegAddr, (uint16_t) len, _buf, (uint16_t) len) != HAL_OK)
+			if(HAL_I2C_Mem_Read_DMA(__Device.__hi2c, BQ25798_I2C_ADDR, MemAddress, MemAddSize, pData, MemAddSize) != HAL_OK)	// transmit onto the I2C bus
 				Error_Handler();
 
-			while(HAL_I2C_IsDeviceReady(&hi2c1, (uint16_t) BQ25798_I2C_ADDR, 5, 50) == HAL_TIMEOUT)
+			// see if the target device is ready
+			while(HAL_I2C_IsDeviceReady(__Device.__hi2c, BQ25798_I2C_ADDR, 5, 50) == HAL_TIMEOUT)
 				;
 
-			while(HAL_I2C_GetState(&hi2c1) != HAL_I2C_STATE_READY)
-				;		// wait for end of transfer
+			// see if the bus is ready
+			while(HAL_I2C_GetState(__Device.__hi2c) != HAL_I2C_STATE_READY)
+				;  // wait for end of transfer
 
 		}
-	while(HAL_I2C_GetError(&hi2c1) == HAL_I2C_ERROR_AF);  // retry on acknowledge failure
+	while(HAL_I2C_GetError(__Device.__hi2c) == HAL_I2C_ERROR_AF);  // retry on acknowledge failure
 }
 
-//
-void __Write(const uint8_t RegAddr, const uint16_t val)
+// I2C read function
+uint16_t _Read(const uint8_t RegAddr)
 {
-	uint8_t _len;  // holds the number of bytes to be transferred via I2C
+	uint8_t temp;  // holds a byte value
 
-	if(val > 0xFF)
-		_len = 2;
-	else
-		_len = 1;
+	if(_RegSize[RegAddr] == 1)  // 1 byte register value - reads uint8_t once
+		{
+			__ReadWrapper(_RegOffset[RegAddr], 1, &temp);  // read one byte from device and put into temp
 
+			_buf[RegAddr] = temp;  // store in register buffer
+		}
+
+	if(_RegSize[RegAddr] == 2)	// 2 byte register value - reads 2 uint8_t in a row and constructs one uint16_t
+		{
+			uint16_t bb;	// holds a 2 byte value
+			__ReadWrapper(_RegOffset[RegAddr], 1, &temp);  // read upper byte from device and put into temp
+			bb = (temp << 8);  // lsh into upper uint16_t byte
+			__ReadWrapper(_RegOffset[RegAddr] + 1, 1, &temp);  // read lower byte from device and put into temp
+			bb |= temp;  // OR temp into lower uint16_t byte
+
+			_buf[RegAddr] = bb;  // store in register buffer
+		}
+
+	return _buf[RegAddr];
+}
+
+// reads out the whole device register
+void _Dump(void)
+{
+	for(uint8_t i = 0; i < REG_CNT; ++i)	// loop over all registers
+		_buf[i] = _Read(i);  // and store in register buffer
+}
+
+// I2C write function
+void _Write(const uint8_t RegAddr, const uint16_t val)
+{
 	do  // write to device
 		{
-			if(HAL_I2C_Mem_Write_DMA(__Device.__hi2c, (uint16_t) BQ25798_I2C_ADDR, RegAddr, _len, (uint8_t*) &val, _len) != HAL_OK)  // transmit
+			if(HAL_I2C_Mem_Write_DMA(__Device.__hi2c, BQ25798_I2C_ADDR, _RegOffset[RegAddr], (uint16_t) _RegSize[RegAddr], (uint8_t*) &val, (uint16_t) _RegSize[RegAddr]) != HAL_OK)  // transmit onto the I2C bus
 				Error_Handler();
 
-			while(HAL_I2C_IsDeviceReady(__Device.__hi2c, (uint16_t) BQ25798_I2C_ADDR, 5, 50) == HAL_TIMEOUT)
+			// see if the target device is ready
+			while(HAL_I2C_IsDeviceReady(__Device.__hi2c, BQ25798_I2C_ADDR, 5, 50) == HAL_TIMEOUT)
 				;
 
+			// see if the bus is ready
 			while(HAL_I2C_GetState(__Device.__hi2c) != HAL_I2C_STATE_READY)
 				;		// wait for end of transfer
 
 		}
 	while(HAL_I2C_GetError(__Device.__hi2c) == HAL_I2C_ERROR_AF);  // retry on acknowledge failure
 }
-;
 
 bq25798_t* bq25798_ctor(I2C_HandleTypeDef *const in_hi2c)
 {
@@ -64,12 +114,8 @@ bq25798_t* bq25798_ctor(I2C_HandleTypeDef *const in_hi2c)
 	__Device.__buffer = _buf;  // point array pointer to internal array
 	__Device.__hi2c = in_hi2c;	// HAL's I2C handler
 
-	__Device.public.Read = &__Read;  // read method
-	__Device.public.Write = &__Write;  // write method
-
-	__Device.public.Write(REG10_Charger_Control_1, 0x07);
-	__Device.public.Read(REG48_Part_Information, 1);
-	__Device.public.Read(REG10_Charger_Control_1, 1);
+	__Device.public.Read = &_Read;  // read method
+	__Device.public.Write = &_Write;  // write method
 
 //	__Device.public.Write(REG10_Charger_Control_1, 0x01);  // reset watchdog, set WD timer to 0.5s
 //	__Device.public.Write(REG14_Charger_Control_5, 0x80);  // set ship FET to enabled
@@ -109,6 +155,5 @@ bq25798_t* bq25798_ctor(I2C_HandleTypeDef *const in_hi2c)
 
 	return (&__Device.public);
 }
-;
 
 bq25798_t *const Device = &__Device.public;  // set pointer to MsgHandler public part
