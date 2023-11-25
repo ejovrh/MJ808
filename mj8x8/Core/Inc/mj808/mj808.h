0e30821618d8c1cbdbde589343635a2f6299fb41
ejovrh cinavi <hrvoje.ivanic@gmx.net>
2023-11-25 15:00:06 +0100

    - WIP for device activity detection

--- mj8x8/Core/Inc/mj808/mj808.h
+++ mj8x8/Core/Inc/mj808/mj808.h
@@ -8,18 +8,20 @@ typedef union  // union for activity indication, see mj8x8_t's _Sleep()
 		/*  0x3F - if any of bits 0 though 5 are set - the device will execute HAL_PWR_EnableSleepOnExit() w. CANbus on
 		 * additionally: if CANBUS_ACTIVE_MASK has bits not set, CANbus will be off
 		 */
-		uint8_t DoHeartbeat :1;  // bit 0 - HeartBeat is running
-		uint8_t CANActive :1;  // CAN is actively being used *is used as a flag to avoid re-entering e.g. __can_go_into_active_mode()
+		// bit0
+		uint8_t DoHeartbeat :1;  // DoHB // HeartBeat is running
+		uint8_t CANActive :1;  // CAN // CAN is actively being used *is used as a flag to avoid re-entering e.g. __can_go_into_active_mode()
 
 		// 0x3C - the device will execute HAL_PWR_EnableSleepOnExit() w. CANbus off
-		uint8_t UtilLEDOn :1;  // utility LED blinking (not shining) is in progress
-		uint8_t ButtonPressed :1;  // some button is being pressed
-		uint8_t HighBeamOn :1;  // high beam is on: PWM - stop mode will break functionality
-		uint8_t FrontLightOn :1;  // front light is on: PWM - stop mode will break functionality
+		uint8_t UtilLEDOn :1;  // uLED // utility LED blinking (not shining) is in progress
+		uint8_t ButtonPressed :1;  // BTN // some button is being pressed
+		uint8_t HighBeamOn :1;  // BEAM // high beam is on: PWM - stop mode will break functionality
+		uint8_t FrontLightOn :1;  // Front // front light is on: PWM - stop mode will break functionality
 
 		// 0xC0 - don't care - the device will execute HAL_PWR_EnterSTOPMode()
 		uint8_t _6 :1;  //
-		uint8_t _7 :1;  // bit 7
+		uint8_t _7 :1;
+	// bit 7
 	};
 	uint8_t byte;  // byte-wise representation of the above bitfield
 } mj808_activity_t;
