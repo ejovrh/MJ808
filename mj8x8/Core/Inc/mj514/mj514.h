e4b71b51f2a6191f23dc3c383ca0bd0f70452a0b
ejovrh cinavi <hrvoje.ivanic@gmx.net>
2024-01-12 08:36:40 +0100

    in mj514.c/.h: #define-d required constants - in _GPIOInit(): defined GPIOs for peripheral device control (motor controller, FeRAM) - in _TimerInit(): defined required timers - implemented timer start and stop functions - in mj514_ctor(): defined required interrupts
    in motor.c:
    - implemented rough start and stop functions for peripheral sleep/wakeup
    
    added mj514_adc.c:
    - copied adc code from mj828 and adapted it to mj514's needs

--- mj8x8/Core/Inc/mj514/mj514.h
+++ mj8x8/Core/Inc/mj514/mj514.h
@@ -28,18 +28,19 @@ typedef union  // union for activity indication, see mj8x8_t's _Sleep()
 
 #include "main.h"
 #if defined(MJ514_)	// if this particular device is active
-// FIXME - define if eventhandler is used
 #define USE_I2C 1	// use I2C
-#define USE_EVENTHANDLER 0	// shall EventHandler code be included
+#define USE_EVENTHANDLER 0	// shall EventHandler code be included -- timer17 is used up for ADC timebase !!!
 
 // FIXME - define activity bit positions
 
 #define TIMER_PRESCALER 799	// global - 8MHz / 799+1 = 10kHz update rate
-#define TIMER3_PERIOD  0xFFFF	//
+#define TIMER2_PERIOD 0xFFFF	//
+#define MOTOR_OFF 0x00	//
+#define TIMER3_PERIOD  0xFF	// TODO - use 0xFFFF
 #define TIMER3_IC1_FILTER	0x0	//
 #define TIMER3_IC2_FILTER 0x0	//
 #define TIMER16_PERIOD 99	// rotary encoder time base - 10ms
-// FIXME - define timer defines
+#define TIMER17_PERIOD 99	// ADC time base - 10ms
 
 typedef enum
 {
@@ -48,33 +49,50 @@ typedef enum
 } rotation_t;
 
 #if USE_I2C
-#include "i2c\i2c.h"
+#include "i2c/i2c.h"
 #endif
 
-#include "mj8x8\mj8x8.h"
+#include "mj8x8/mj8x8.h"
 #include "gear.h" // Rohloff electronic shifting unit
+#include "adc/adc.h"
+
+#define VREFINT_CAL *((uint16_t*) ((uint32_t) 0x1FFFF7BA)) // value is 1525 - internal reference voltage calibration data: acquired by measuring Vdda = 3V3 (+-10%) at 30 DegC (+-5 DegC), see RM0091l paragraph 13.8, p 260 for conversion formula
+
+#define ADC_CHANNELS 2	// how many ADC channels are we using
+#define ADC_CHANNEL_MOTOR_IPROP ADC_CHANNEL_4 // PA4
 
 // definitions of device/PCB layout-dependent hardware pins
 #define TCAN334_Standby_Pin GPIO_PIN_5	//	defined here but initialised in mj8x8.c
 #define TCAN334_Standby_GPIO_Port GPIOA	//	defined here but initialised in mj8x8.c
-#define Rotary_A_Pin GPIO_PIN_6
+
+#define Motor_IN1_Pin GPIO_PIN_0	// motor direction control PWM output signal
+#define Motor_IN1_GPIO_Port GPIOA
+#define Motor_IN2_Pin GPIO_PIN_1	// motor direction control PWM output signal
+#define Motor_IN2_GPIO_Port GPIOA
+#define Motor_FLT_Pin GPIO_PIN_2	// motor controller fault interrupt pin
+#define Motor_FLT_GPIO_Port GPIOA
+#define Motor_FLT_EXTI_IRQn EXTI2_3_IRQn
+#define Motor_SLP_Pin GPIO_PIN_3	// motor controller sleep pin: low - sleep, high - wake
+#define Motor_SLP_GPIO_Port GPIOA
+#define Motor_IPROP_Pin GPIO_PIN_4	// motor current monitoring ADC channel 4
+#define Motor_IPROP_GPIO_Port GPIOA
+#define FeRAM_WP_Pin GPIO_PIN_5	// FeRAM write-protect
+#define FeRAM_WP_GPIO_Port GPIOA
+#define Rotary_A_Pin GPIO_PIN_6	// rotary encoder A/B input for detection/measurement of gear rotation
 #define Rotary_A_GPIO_Port GPIOA
-#define Rotary_B_Pin GPIO_PIN_7
+#define Rotary_B_Pin GPIO_PIN_7	// rotary encoder A/B input for detection/measurement of gear rotation
 #define Rotary_B_GPIO_Port GPIOA
 #define I2C_SDA_Pin GPIO_PIN_10	// see i2c_ctor()
 #define I2C_SDA_GPIO_Port GPIOA	// see i2c_ctor()
 #define I2C_SCL_Pin GPIO_PIN_11
-#define I2C_SCL_GPIO_Port GPIOA
-
-// FIXME - define GPIOs
-#define Motor_Direction_Down_Pin GPIO_PIN_0
-#define Motor_Direction_Down_GPIO_Port GPIOA
-#define Motor_Direction_Up_Pin GPIO_PIN_0
-#define Motor_Direction_Up_GPIO_Port GPIOA
-#define FeRAM_WP_Pin GPIO_PIN_0
-#define FeRAM_WP_GPIO_Port GPIOA
-
+#define I2C_GPIO_Port GPIOA
 // definitions of device/PCB layout-dependent hardware pins
+
+enum mj514_adcchannels
+{  // order is important! - this MCU has no ranking and the order of channels is by channel number
+	  Iprop,  // Motor Iprop - PA4 - proportional motor current
+	  Vrefint  // internal reference voltage
+};
 
 typedef struct	// struct describing devices on mj514
 {
