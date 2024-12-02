c2521a3f97881059d0e7b3c01d650d93935dbab5
ejovrh cinavi <hrvoje.ivanic@gmx.net>
2024-12-02 12:25:07 +0100

    - added missing TCAN334_Standby_Pin/TCAN334_Standby_GPIO_Port (A5) into template project

--- mj8x8-template/Core/Inc/main.h
+++ mj8x8-template/Core/Inc/main.h
@@ -57,6 +57,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define TCAN334_Standby_Pin GPIO_PIN_5
+#define TCAN334_Standby_GPIO_Port GPIOA
 
 /* USER CODE BEGIN Private defines */
 
