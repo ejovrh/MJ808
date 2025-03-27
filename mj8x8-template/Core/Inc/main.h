c66d28df711f6daac8bc85c4b8c78a9c95ebf13e
ejovrh cinavi <hrvoje.ivanic@gmx.net>
2025-03-27 07:43:04 +0100

    - reverted mj8x8-template.ioc back to a base config (from some unfinished half-config for testing purposes)

--- mj8x8-template/Core/Inc/main.h
+++ mj8x8-template/Core/Inc/main.h
@@ -57,8 +57,6 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define TCAN334_Standby_Pin GPIO_PIN_5
-#define TCAN334_Standby_GPIO_Port GPIOA
 
 /* USER CODE BEGIN Private defines */
 
